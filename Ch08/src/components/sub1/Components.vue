3b0c13d9f4b8601281a8876951fdc7aeaaed5596
JGwanghou <jogwangho0619@gmail.com>
2023-04-18 16:50:36 +0900

    0418 reUpload

---
+++ Ch08/src/components/sub1/Components.vue
@@ -0,0 +1,77 @@
+<template>
+  <h4>Vuetity Components 실습</h4>
+
+  <div>
+    <h4>Icons</h4>
+    <v-icon color="warning" icon="mdi-wifi" size="x-large"></v-icon>
+    <v-icon color="info" icon="mdi-check-circle" size="x-large"></v-icon>
+    <v-icon icon="mdi-cat"></v-icon>
+    <hr />
+    <h4>Images</h4>
+    <v-img
+      :width="324"
+      aspect-ratio="16/9"
+      cover
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+    <v-img
+      :width="397"
+      aspect-ratio="1/1"
+      cover
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+    <hr />
+    <h4>Buttons</h4>
+    <v-btn variant="elevated" color="primary"> 구입 </v-btn>
+    <v-btn prepend-icon="mdi-cat" variant="outlined"> Button </v-btn>
+    <v-btn block>Block Button</v-btn>
+    <v-btn color="secondary">Button4</v-btn>
+    <v-btn color="success">Button5</v-btn>
+    <v-btn color="warning">Button6</v-btn>
+    <hr />
+    <h4>Card</h4>
+    <v-card
+      loading
+      title="제목입니다"
+      subtitle="부제목입니다"
+      text="내용입니다."
+    >
+      <v-card-actions>
+        <v-btn>확인</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card
+      loading
+      width="400"
+      title="제목입니다"
+      subtitle="부제목입니다"
+      text="내용입니다."
+      class="border mt-2"
+    >
+      <v-card-actions>
+        <v-btn>확인</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card width="400">
+      <v-img
+        height="200"
+        src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+        cover
+        class="text-white my-10"
+      >
+        <v-card-item>
+          <v-card-title>제목입니다</v-card-title>
+
+          <v-card-subtitle>부제목입니다</v-card-subtitle>
+        </v-card-item>
+      </v-img>
+
+      <v-card-text> 여기는 내용입니다.</v-card-text>
+    </v-card>
+    <hr />
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
---
+++ Ch08/src/components/sub3/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <h4>Vuetity Layout 실습</h4>
+
+  <v-app>
+    <v-app-bar> 헤더영역 </v-app-bar>
+    <v-main>
+      <v-navigation-drawer>사이드</v-navigation-drawer>
+      <v-container>메인 영역</v-container>
+    </v-main>
+    <v-footer app color="primary">푸터 영역</v-footer>
+  </v-app>
+</template>
+<script setup></script>
+<style scoped></style>
